e170688c95e816ee3d559d316e80187f3600a019
Colin Tondreau <cdt8588@rit.edu>
2024-03-18 22:24:53 -0400

    terraform script for amplify added, individual changes to git hub token and source code repo must be added

---
+++ amplify/main.tf
@@ -0,0 +1,21 @@
+resource "aws_amplify_app" "tut_amplify_app" {
+    name = var.app_name
+    repository = var.repository
+    oauth_token = var.token
+}
+
+resource "aws_amplify_branch" "tut_amplify_branch" {
+    app_id = aws_amplify_app.tut_amplify_app.id
+    branch_name = var.branch_name
+}
+
+resource "aws_amplify_domain_association" "tut_domain_association" {
+    app_id = aws_amplify_app.tut_amplify_app.id
+    domain_name = var.domain_name
+    wait_for_verification = false
+
+    sub_domain {
+        branch_name = aws_amplify_branch.tut_amplify_branch.branch_name
+        prefix = var.branch_name
+    }
+}
