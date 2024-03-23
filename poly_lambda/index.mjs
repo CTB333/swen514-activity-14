50eef8fe064635820f9a840e8b8d815246b59f5e
Colin Tondreau <cdt8588@rit.edu>
2024-03-23 16:04:31 -0400

    Successfully converts text to speech and returns a url to access the mp3 file

--- poly_lambda/index.mjs
+++ poly_lambda/index.mjs
@@ -1,74 +1,54 @@
 import { v4 as uuidv4 } from "uuid";
 import AWS from "aws-sdk";
 
+AWS.config.update({ region: "us-east-1" });
+
 let polly = new AWS.Polly();
 let s3 = new AWS.S3();
 
-export const handler = async (event, context, callback) => {
+export const handler = async (event, context) => {
+  let success = true;
+  let url = null;
+
   const pollyParams = {
     OutputFormat: "mp3",
     Text: event.text,
     VoiceId: "Matthew",
   };
-  // 1. Getting the audio stream for the text that user entered
-  polly
-    .synthesizeSpeech(pollyParams)
-    .on("success", function (response) {
-      let data = response.data;
-      let audioStream = data.AudioStream;
-      let key = uuidv4();
-      let s3BucketName = "test-polly-bucket";
 
-      console.log(`Poly successfull`);
+  try {
+    let data = await polly.synthesizeSpeech(pollyParams).promise();
 
-      // 2. Saving the audio stream to S3
-      let params = {
-        Bucket: s3BucketName,
-        Key: key + ".mp3",
-        Body: audioStream,
-      };
-      s3.putObject(params)
-        .on("success", function (response) {
-          console.log("S3 Successful");
-        })
-        .on("complete", function () {
-          console.log("S3 Done");
+    let audioStream = data.AudioStream;
+    let key = uuidv4();
+    let s3BucketName = "test-polly-bucket";
+    let fileName = key + ".mp3";
 
-          let s3params = {
-            Bucket: s3BucketName,
-            Key: key + ".mp3",
-          };
-          // 3. Getting a signed URL for the saved mp3 file
-          let url = s3.getSignedUrl("getObject", s3params);
-          // Sending the result back to the user
-          let result = {
-            bucket: s3BucketName,
-            key: key + ".mp3",
-            url: url,
-          };
-          callback(null, {
-            statusCode: 200,
-            headers: {
-              "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify(result),
-          });
-        })
-        .on("error", function (response) {
-          console.log(`S3 error: ${err}`);
-        })
-        .send();
-    })
-    .on("error", function (err) {
-      console.log(`Poly error: ${err}`);
+    let uploadParams = {
+      Bucket: s3BucketName,
+      Key: fileName,
+      Body: audioStream,
+    };
 
-      callback(null, {
-        statusCode: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify(err),
-      });
-    })
-    .send();
+    await s3.upload(uploadParams).promise();
+
+    let signUrlParams = {
+      Bucket: s3BucketName,
+      Key: fileName,
+    };
+
+    url = s3.getSignedUrl("getObject", signUrlParams);
+  } catch (err) {
+    console.log(`Polly Error: ${err}`);
+
+    success = false;
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ success, url }),
+  };
 };
